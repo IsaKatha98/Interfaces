c80075cf172825c0967f77f0bd5f326f3cd279d7
IsaKatha98 <113983281+IsaKatha98@users.noreply.github.com>
2023-10-05 12:14:47 +0200

    Organizado

---
+++ MAUI/HolaMundo_Tema05/HolaMundo_Tema05/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HolaMundo_Tema05
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MAUI/HolaMundo_Tema05/HolaMundo_Tema05/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HolaMundo_Tema05
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
