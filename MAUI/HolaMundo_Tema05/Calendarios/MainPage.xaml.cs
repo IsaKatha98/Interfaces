3df0ed4b84c03f410472e8b709df5368282974db
IsaKatha98 <113983281+IsaKatha98@users.noreply.github.com>
2023-10-10 13:12:53 +0200

    El ejercicio 4 a medias

---
+++ MAUI/HolaMundo_Tema05/Calendarios/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Calendarios
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+     
+    }
+}
