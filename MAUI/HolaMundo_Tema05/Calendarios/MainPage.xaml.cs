1187a0184559938d961fa231510e7aa81dc8fda3
IsaKatha98 <isabel.loerzer@iesnervion.es>
2023-10-13 00:19:18 +0200

    Terminado el primer boletín

--- MAUI/HolaMundo_Tema05/Calendarios/MainPage.xaml.cs
+++ MAUI/HolaMundo_Tema05/Calendarios/MainPage.xaml.cs
@@ -8,6 +8,12 @@
             InitializeComponent();
         }
 
-     
+        private void elegirFecha(object sender, DateChangedEventArgs e)
+        {
+            // Cuando se selecciona una fecha de entrada, habilitar el DatePicker de salida
+            fechaSalida.MinimumDate = fechaEntrada.Date;
+            fechaSalida.IsEnabled = true;
+        }
+
     }
 }
