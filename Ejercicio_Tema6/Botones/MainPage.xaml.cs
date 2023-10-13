eb1ece2aab9e8d8006b82224e4ce4ab0798c85bf
IsaKatha98 <113983281+IsaKatha98@users.noreply.github.com>
2023-10-13 09:39:22 +0200

    Ejercicio de botones terminado

--- Ejercicio_Tema6/Botones/MainPage.xaml.cs
+++ Ejercicio_Tema6/Botones/MainPage.xaml.cs
@@ -1,29 +1,60 @@
-﻿using Org.Apache.Http.Conn;
+﻿
 
 namespace Botones
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+
+        Boolean btnYaCreado = false;
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void evento_Clicked(object sender, EventArgs e)
+        private void evento_ClickedBtn2(object sender, EventArgs e)
         {
-            Button btn3 = new Button {
+            //creamos un botón nuevo.
 
-                Text = "Botón 3",
-                HorizontalOptions = LayoutOptions.Center,
-                VerticalOptions = LayoutOptions.Center,
-                BackgroundColor = Colors.LightBlue,
+            if (btnYaCreado!=true)
+            {
 
+                Button btn3 = new Button 
+                {
+
+                    Text = "Botón 3",
+                    HorizontalOptions = LayoutOptions.Center,
+                    VerticalOptions = LayoutOptions.Center,
+                    BackgroundColor = Colors.LightBlue,
+                    HeightRequest = 70,
+                    MinimumWidthRequest = 200,
+                    FontFamily = "Verdana",
+                    FontSize = 16,
+                    FontAttributes = FontAttributes.Bold,
+                    BorderColor = Colors.Yellow,
+                    Margin = 30
+
+                };
+
+                //añadimos un botón y añadimos el evento.
+
+                vslButtons.Add(btn3);
+                btn3.Clicked += evento_ClickedBtn3;
+                btnYaCreado = true;
 
             }
+           
 
     
+        }
+
+        private void evento_ClickedBtn3 (object sender, EventArgs e)
+        {
+            vslButtons.Remove(btn1);
+           // btn2.WidthRequest = 500;
+            btn2.Text = "Crear controles en tiempo de ejecución mola";
+
+
         }
 
 
---
+++ Ejercicio_Tema6/Botones/obj/Debug/net7.0-android/android/src/crc6490ad35069f742cc5/MainActivity.java
@@ -0,0 +1,48 @@
+package crc6490ad35069f742cc5;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("Botones.MainActivity, Botones", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("Botones.MainActivity, Botones", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("Botones.MainActivity, Botones", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
