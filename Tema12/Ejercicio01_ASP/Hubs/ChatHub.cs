4c67f8d8868922688214ca6ba30eba89935272dc
IsaKatha98 <113983281+IsaKatha98@users.noreply.github.com>
2024-01-16 14:37:53 +0100

    tema 12 empezado

---
+++ Tema12/Ejercicio01_ASP/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Models;
+
+namespace Ejercicio01_ASP.Hubs
+{
+    public class ChatHub:Hub
+    {
+        public async Task SendMessage(clsMensajeUsuario mensajeU)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", mensajeU);
+        }
+    }
+}
---
+++ Tema12/Models/clsMensajeUsuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class clsMensajeUsuario
+    {
+        string nombreUsuario;
+        string mensajeUsuario;
+        public clsMensajeUsuario() { }
+
+        public string NombreUsuario
+        {
+            get { return nombreUsuario; }
+            set { nombreUsuario = value; }
+        }
+
+        public string MensajeUsuario
+        {
+            get { return mensajeUsuario; }
+            set { mensajeUsuario = value; }
+        }
+    }
+}
