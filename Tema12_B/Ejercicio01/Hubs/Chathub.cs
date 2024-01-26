acdf8a8432fd41b20ab93e128d980f21c5c1d27c
IsaKatha98 <113983281+IsaKatha98@users.noreply.github.com>
2024-01-26 10:22:54 +0100

    segundo intento de signalr

---
+++ Tema12_B/Ejercicio01/Hubs/Chathub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Ejercicio01.Hubs
+{
+    public class Chathub: Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
