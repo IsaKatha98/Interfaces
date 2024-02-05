cd2d438d4510f3e7082e651730edcd48aa7a4ec6
IsaKatha98 <113983281+IsaKatha98@users.noreply.github.com>
2024-02-05 13:22:42 +0100

    mieda

--- Tema11/ClassLibrary1/obj/Debug/net7.0/BL.AssemblyInfo.cs
+++ Tema11/ClassLibrary1/obj/Debug/net7.0/BL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acdf8a8432fd41b20ab93e128d980f21c5c1d27c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
