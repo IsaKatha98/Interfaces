da9db61dc4fbb24da6d90089a83d33bcfe4c6c52
IsaKatha98 <113983281+IsaKatha98@users.noreply.github.com>
2024-02-05 09:12:31 +0100

    intentando trivia

---
+++ PokeTrivia/PokeTrivia/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PokeTrivia
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ PokeTrivia/PokeTrivia/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PokeTrivia
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
